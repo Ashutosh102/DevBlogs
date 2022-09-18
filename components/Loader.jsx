24040161d2c2e0feb6fd6384175c496edd756321
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2022-09-18 07:09:39 +0530

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,6 +4,7 @@ const Loader = () => (
   <div className="text-center">
     <button
       type="button"
+      // eslint-disable-next-line max-len
       className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-rose-600 hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700 transition ease-in-out duration-150 cursor-not-allowed"
       disabled=""
     >
